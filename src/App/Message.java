badd70abd9ee9739543ca7ed3879aa76e7497c98
Arthur Chevalier <arthur.a.chevalier@gmail.com>
2023-02-26 19:04:34 +0100

    Immplémentation de la fonctionnalité replyTo et isRepublished à la classe Message

--- src/App/Message.java
+++ src/App/Message.java
@@ -4,6 +4,8 @@ public class Message {
     private String content;
     private int id;
     private static final int CONTENT_MAX_SIZE = 256;
+    private int replyTo = -1;
+    private boolean republished = false;
 
     public Message(String content, int id) throws Exception {
         if (content.length() > CONTENT_MAX_SIZE) {
@@ -13,8 +15,6 @@ public class Message {
         this.id = id;
     }
 
-
-
     public String getContent() {
         return content;
     }
@@ -23,6 +23,21 @@ public class Message {
         return id;
     }
 
+    public int getReplyTo() {
+        return replyTo;
+    }
+
+    public boolean isRepublished() {
+        return republished;
+    }
+
+    public void setReplyTo(int replyTo) {
+        this.replyTo = replyTo;
+    }
+
+    public void setRepublished(boolean republished) {
+        this.republished = republished;
+    }
 
     public String toString() {
         return "Message n°" + id + " : " + content;
